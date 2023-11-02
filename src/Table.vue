7269a8e9e25bd89c660bf3bc194cd7abb1dd59be
boris <borisenko_daniil@inbox.ru>
2023-11-02 20:42:01 +0300

    queries are work

--- src/Table.vue
+++ src/Table.vue
@@ -3,7 +3,7 @@
     <button @click="getMetaData">Запрос метаданных</button>
     <button @click="getFieldValues">Запрос значений полей</button>
     <button @click="getCube">Запрос куба</button>
-
+    <pre>{{data}}</pre>
     <div v-if="loading">Загрузка...</div>
   </div>
 </template>
@@ -15,15 +15,17 @@ export default {
   name: 'query',
   data() {
     return {
-      loading: false
+      data: {},
+      loading: false,
     };
   },
   methods: {
     getMetaData() {
       this.loading = true;
-      axios.post('/v1/report-job/get-metadata')
+      axios.post('/v1/report-job/get-metadata', {})
           .then(response => {
             // Обработка ответа
+            this.data = response.data.data;
             console.log(response.data);
           })
           .catch(error => {
@@ -36,9 +38,10 @@ export default {
     },
     getFieldValues() {
       this.loading = true;
-      axios.post('/v1/olap/get-field-values')
+      axios.post('/v1/olap/get-field-values', {})
           .then(response => {
             // Обработка ответа
+            this.data = response.data;
             console.log(response.data);
           })
           .catch(error => {
@@ -51,9 +54,10 @@ export default {
     },
     getCube() {
       this.loading = true;
-      axios.post('/v1/olap/get-cube')
+      axios.post('/v1/olap/get-cube', {})
           .then(response => {
             // Обработка ответа
+            this.data = response.data;
             console.log(response.data);
           })
           .catch(error => {
